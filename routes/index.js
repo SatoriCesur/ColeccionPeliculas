fea62be91f6df47321057d97c22414508d9b7b88
Satori <satoricesurformacion@gmail.com>
2025-11-14 19:48:04 +0100

    Página de soporte agregada

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Colección de Peliculas' });
 });
 
+/* GET sopport page. */
+router.get('/soporte', function(req, res, next) {
+  res.render('soporte', { title: 'Soporte' });
+});
+
 module.exports = router;

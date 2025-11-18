e226c6209d6b40b981a761b723335c6fd03ec2f1
SatoriCesur <satoricesurformacion@gmail.com>
2025-11-18 11:27:01 +0100

    Agrega modal de detalles de película y mejora la estructura del código en index.ejs, index.js y estilos CSS

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,4 @@
-const bRegistro = document.querySelector(".boton-registro"); 
+const bRegistro = document.querySelector(".boton-registro");
 const contenedorRegistro = document.querySelector(".contenedor_registro");
 const cerrar = document.querySelector(".cerrar");
 const registro = document.querySelector(".registro");
@@ -39,16 +39,40 @@ contenedorAcceso?.addEventListener("click", (e) => {
 });
 
 // --- CAMBIAR PELICULAS ---
-const peliculas = ["/images/superman_fondo.jpg", 
-    "images/7ed2fe4a-2799-499b-89e8-ab4f05eeb214_source-aspect-ratio_1600w_0.jpg",
-    "images/personajes-jumanji-el-siguiente-nivel-4191.jpg","/images/compose.webp",];
+const peliculas = [
+  "/images/superman_fondo.jpg",
+  "images/7ed2fe4a-2799-499b-89e8-ab4f05eeb214_source-aspect-ratio_1600w_0.jpg",
+  "images/personajes-jumanji-el-siguiente-nivel-4191.jpg",
+  "/images/compose.webp",
+];
 let indicePelicula = 0;
 const contenedor = document.querySelector(".contenedor1");
 const botonSiguiente = document.querySelector(".icono");
 botonSiguiente?.addEventListener("click", () => {
-    indicePelicula = (indicePelicula + 1) % peliculas.length;
-    contenedor.style.backgroundImage = `linear-gradient(0deg, rgb(0, 0, 0, 0.5), rgb(0, 0, 0, 0.5)),url('${peliculas[indicePelicula]}')`;
-    contenedor.style.backgroundSize = 'cover';
-    contenedor.style.backgroundPosition = 'center';
-    contenedor.style.backgroundRepeat = 'no-repeat';
+  indicePelicula = (indicePelicula + 1) % peliculas.length;
+  contenedor.style.backgroundImage = `linear-gradient(0deg, rgb(0, 0, 0, 0.5), rgb(0, 0, 0, 0.5)),url('${peliculas[indicePelicula]}')`;
+  contenedor.style.backgroundSize = "cover";
+  contenedor.style.backgroundPosition = "center";
+  contenedor.style.backgroundRepeat = "no-repeat";
+});
+
+// --- MODAL DETALLES ---
+const detallesBotones = document.querySelectorAll(".boton-detalles");
+const modalDetalles = document.querySelector(".modal_detalles");
+const cerrarDetalles = document.querySelector(".cerrar-detalles");
+
+detallesBotones.forEach(boton => {
+  boton.addEventListener("click", () => {
+    modalDetalles.classList.add("detalles_activo");
+  });
+});
+
+cerrarDetalles?.addEventListener("click", () => {
+  modalDetalles.classList.remove("detalles_activo");
+});
+
+modalDetalles?.addEventListener("click", (e) => {
+  if (e.target === !modalDetalles) {
+    modalDetalles.classList.remove("detalles_activo");
+  }
 });
